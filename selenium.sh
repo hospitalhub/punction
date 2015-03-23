674a4fd52d49f32307f6f2484a6525161314b800
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-03-23 10:43:51 +0100

    selenium in travis

--- selenium.sh
+++ selenium.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # selenium.sh: Start Selenium up and also start headless screen.
 #Xvfb needed: sudo apt-get install xvfb 
-Xvfb :99 -ac &
-export DISPLAY=:99 
+#Xvfb :99 -ac &
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+sleep 3
 java -jar vendor/netwing/selenium-server-standalone/selenium-server-standalone-2.43.0.jar &
+sleep 3
