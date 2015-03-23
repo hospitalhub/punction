5ef92636bcd28f34ad6d52405e75a0a79292526c
amarcinkowski <andrzej.marcinkowski@gmail.com>
2015-03-23 09:42:11 +0100

    xvfb comment

--- selenium.sh
+++ selenium.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # selenium.sh: Start Selenium up and also start headless screen.
+#Xvfb needed: sudo apt-get install xvfb 
 Xvfb :99 -ac &
 export DISPLAY=:99 
 java -jar vendor/netwing/selenium-server-standalone/selenium-server-standalone-2.43.0.jar &
